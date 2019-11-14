c467993694fe258f761ba606062c2f265d08d82d
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2019-11-14 19:53:49 +0100

    Remove the Application and its supervision tree

--- mix.exs
+++ mix.exs
@@ -14,8 +14,7 @@ defmodule Sandbox.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
-      mod: {Sandbox.Application, []}
+      extra_applications: [:logger]
     ]
   end
 
@@ -24,8 +23,6 @@ defmodule Sandbox.MixProject do
     [
       {:gen_stage, "~> 0.14.3"},
       {:elixir_uuid, "~> 1.2"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 end
